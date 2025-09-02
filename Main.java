86e8350106c0e00f65598e165e09db598e098c7e
fatiminn <anahi08585@gmail.com>
2025-09-02 04:55:13 -0600

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("🌍 Bienvenido al proyecto 'Ambiente y Sustentabilidad'");
+        System.out.println("Este repositorio contiene un sitio web en HTML y CSS con un resumen estructurado.");
+        System.out.println("Creadora: Fátima Alejandro – Ingeniería en Manufactura");
+    }
+}
